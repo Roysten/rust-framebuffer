c5d9adce93afcec3f9972e5b49029eaa8b536a45
Legokichi Duckscallion <legokichi@gmail.com>
2023-06-06 22:45:32 +0900

    bump image

--- examples/rust-logo/main.rs
+++ examples/rust-logo/main.rs
@@ -1,6 +1,3 @@
-extern crate bmp;
-extern crate framebuffer;
-
 use framebuffer::{Framebuffer, KdMode};
 
 fn main() {
@@ -12,18 +9,25 @@ fn main() {
     let bytespp = framebuffer.var_screen_info.bits_per_pixel / 8;
 
     let mut frame = vec![0u8; (line_length * h) as usize];
-    let img = bmp::open("examples/rust-logo/rust-logo.bmp").unwrap();
+
+    let img = image::io::Reader::open("examples/rust-logo/rust-logo.bmp")
+        .unwrap()
+        .decode()
+        .unwrap();
 
     //Disable text mode in current tty
     let _ = Framebuffer::set_kd_mode(KdMode::Graphics).unwrap();
 
-    for offset in 0..w - img.get_width() {
-        for (x, y) in img.coordinates() {
-            let px = img.get_pixel(x, y);
-            let start_index = (y * line_length + (offset + x) * bytespp) as usize;
-            frame[start_index] = px.b;
-            frame[start_index + 1] = px.g;
-            frame[start_index + 2] = px.r;
+    for offset in 0..w - img.width() {
+        for x in 0..img.width() {
+            for y in 0..img.height() {
+                use image::GenericImageView;
+                let px: image::Rgba<u8> = img.get_pixel(x, y);
+                let start_index = (y * line_length + (offset + x) * bytespp) as usize;
+                frame[start_index] = px.0[0];
+                frame[start_index + 1] = px.0[1];
+                frame[start_index + 2] = px.0[2];
+            }
         }
 
         framebuffer.write_frame(&frame);
